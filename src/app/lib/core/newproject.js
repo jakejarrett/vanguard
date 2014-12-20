43fa75151018a0b77f69e832ae9b542f16088ffb
Jake <jakehh@live.com.au>
2014-12-20 19:55:59 +1100

    Cleaning up newproject
    Preparing newproject for Better code & Cleaned up unused old code.

--- src/app/lib/core/newproject.js
+++ src/app/lib/core/newproject.js
@@ -1,13 +1,3 @@
-/*
-var fs = require('fs');
-
-var obj;
-
-fs.readFile('file', 'utf8', function (err, data) {
-    if (err) throw err;
-    obj = JSON.parse(data);
-});
-*/
 var defaultProject =
     '{ "projectInfo" :' +
         '{"tempo":"120","tracks":"4","name":"Untitled Project","location":"~/Projects/Project Name","effects":' +
@@ -24,8 +14,11 @@ module.exports.newProject = defaultProject;
 
 // New Project (This shouldn't be exported, So it should be fine to leave it as is for now)
 var Project = JSON.parse(defaultProject),
+    bpm = Project['projectInfo'].tempo,
+    ProjectTitle = Project['projectInfo'].title,
+    ProjectLocation = Project['projectInfo'].location,
+    trackNumbers = Number(Project['projectInfo'].tracks);
 
-bpm = Project['projectInfo'].tempo,
-ProjectTitle = Project['projectInfo'].title,
-ProjectLocation = Project['projectInfo'].location,
-trackNumbers = Number(Project['projectInfo'].tracks);
+// module.exports = function() {
+//         have all the new project stuff in here
+// }
