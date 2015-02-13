513f148831ff6e96f7176106b46c37f1c1a58f4c
Jake <jakehh@live.com.au>
2015-02-13 19:49:26 +1100

    Added gulp to build vanguard
    Gulp now builds vanguard for win, OS X, linux.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    install = require("gulp-install"),
+    util = require('gulp-util'),
+    NwBuilder = require('node-webkit-builder'),
+
+    nw = new NwBuilder({
+        files: ['*', '!./*.app', './**', '!./cache/**', '!./build/**'], // use the glob format
+        platforms: ['win', 'osx', 'linux']
+    });
+
+
+gulp.src(['./package.json'])
+  .pipe(install());
+
+gulp.task('default', function() {
+  // place code for your default task here
+
+  nw.on('log',  console.log);
+
+    // Build returns a promise
+    nw.build().then(function () {
+       console.log('all done!');
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+});
