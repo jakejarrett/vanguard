80ef2283444999a49d62a237c6e8eee818a51aff
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-07-12 21:45:12 +1000

    Delete AppLayout.js
    Not used

--- app/assets/js/AppLayout.js
+++
@@ -1,9 +0,0 @@
-import Marionette from "backbone.marionette";
-import layoutTemplate from "./templates/layout.hbs";
-
-export default class AppLayout extends Marionette.LayoutView {
-  constructor(...args) {
-    super(...args);
-    this.template = layoutTemplate;
-  }
-}
